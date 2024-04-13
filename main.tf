a5d619baccbd7ecaad0441cffc6cba6a1c45db0a
Richard Jones <richard.jones@cloudtechnologyoffice.com>
2024-04-13 15:16:12 +0100

    ADjustments

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 resource "azurerm_resource_group" "default" {
-  name     = "testcluster"
-  location = "polandcentral"
+  name     = "aks"
+  location = var.region
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
-  name                = "testcluster"
+  name                = var.cluster_name
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
-  dns_prefix          = "test-cluster"
+  dns_prefix          = var.cluster_name
 
   default_node_pool {
-    name       = "test"
+    name       = "ess_deployed"
     node_count = 1
     vm_size    = "Standard_DS2_v2"
   }
@@ -18,4 +18,15 @@ resource "azurerm_kubernetes_cluster" "default" {
   identity {
     type = "SystemAssigned"
   }
+}
+
+variable "region" {
+    type = string
+    default = "West Europe"
+    description = "Where would you like to deploy your cluster?"
+}
+
+variable "cluster_name" {
+    type = string
+    description = "What would you like your cluster to be?"
 }
